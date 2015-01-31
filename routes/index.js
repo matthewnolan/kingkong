8e502a777511fe349efb826b3bf6743f0f925d86
Andy Hayes <andygoestohollywood@gmail.com>
2015-01-31 13:27:54 +0000

    king kong error view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/*
+ currently this file is unused... We may wish to use it instead of index.hmtl if we intend to use a view engine.
+ Right now the index.html inside public/ is served by the res.sendFile call
+*
+* */
 router.get('/', function (req, res, next) {
-	//res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 
 });
 
